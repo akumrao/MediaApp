803ef57e9b6c9a6c05d2b6d0014b3482bde7f814
Arvind Umrao <arvind.umrao@sococo.com>
2019-11-23 19:48:17 +0530

    added log at c++ for Logcat

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,26 @@
 #include <jni.h>
 #include <string>
 
+
+#include <android/log.h>
+
+#include <android/log.h>
+
+static const char* kTAG = "hello-jniCallback";
+#define LOGI(...) \
+  ((void)__android_log_print(ANDROID_LOG_INFO, kTAG, __VA_ARGS__))
+#define LOGW(...) \
+  ((void)__android_log_print(ANDROID_LOG_WARN, kTAG, __VA_ARGS__))
+#define LOGE(...) \
+  ((void)__android_log_print(ANDROID_LOG_ERROR, kTAG, __VA_ARGS__))
+
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_harman_vns_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C1++";
+
+    LOGE("Failed to AttachCurrentThread, ErrorCode = %d", 100);
+
     return env->NewStringUTF(hello.c_str());
 }
