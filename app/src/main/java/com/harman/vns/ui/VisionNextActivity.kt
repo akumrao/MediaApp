7416c632246dff57179129f5dc93814f987cd256
Parashurama G D <parashurama.govind@harman.com>
2019-12-02 12:48:51 +0530

    Added ping and download fragment.

--- app/src/main/java/com/harman/vns/ui/VisionNextActivity.kt
+++ app/src/main/java/com/harman/vns/ui/VisionNextActivity.kt
@@ -2,16 +2,54 @@ package com.harman.vns.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
 import androidx.navigation.Navigation.findNavController
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import com.harman.vns.R
+import kotlinx.android.synthetic.main.activity_vison_next.drawerLayout
+import kotlinx.android.synthetic.main.activity_vison_next.navigationView
+import kotlinx.android.synthetic.main.activity_vison_next.toolbar
 
 class VisionNextActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_vison_next)
+
+        setSupportActionBar(toolbar)
+
+        setupNavigation()
     }
 
-    override fun onSupportNavigateUp() = findNavController(this,
-            R.id.navHostFragment).navigateUp()
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navigateUp(findNavController(this,
+                R.id.nav_host_fragment), drawerLayout)
+    }
+
+    private fun setupNavigation() {
+        val navController = findNavController(this, R.id.nav_host_fragment)
+
+        // Update action bar to reflect navigation
+        setupActionBarWithNavController(this, navController, drawerLayout)
+
+        // Handle nav drawer item clicks
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            drawerLayout.closeDrawers()
+            true
+        }
+
+        // Tie nav graph to items in nav drawer
+        setupWithNavController(navigationView, navController)
+    }
 }
---
+++ app/src/main/java/com/harman/vns/ui/WelcomeFragment.kt
@@ -0,0 +1,33 @@
+package com.harman.vns.ui
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.harman.vns.R
+
+class WelcomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+    }
+
+    override fun onDetach() {
+        super.onDetach()
+    }
+}
