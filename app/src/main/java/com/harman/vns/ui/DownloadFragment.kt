b05f3816344ca834a5b61767fd7b3b12fa6a9258
Parashurama G D <parashurama.govind@harman.com>
2019-12-10 15:35:03 +0530

    Added download fragment

--- app/src/main/java/com/harman/vns/ui/DownloadFragment.kt
+++ app/src/main/java/com/harman/vns/ui/DownloadFragment.kt
@@ -5,10 +5,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.harman.vns.R
 
 class DownloadFragment : Fragment() {
+
+    private external fun startTicks(name: String?)
+    private external fun StopTicks()
+
+    lateinit var rootView: View
+    lateinit var spinner: Spinner
+    lateinit var serverUrlId: TextView
+    lateinit var filesizes:Array<String>
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +34,34 @@ class DownloadFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_download, container, false)
+        rootView = inflater.inflate(R.layout.fragment_download, container, false)
+        initView()
+        return rootView
+    }
+
+    private fun initView() {
+        spinner = rootView.findViewById(R.id.fileSizeSpinnerId)
+        serverUrlId = rootView.findViewById(R.id.serverUrlId)
+        filesizes = resources.getStringArray(R.array.filesize)
+
+        val adapter = activity?.let {
+            ArrayAdapter(it,
+                android.R.layout.simple_spinner_item, filesizes)
+        }
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>,
+                view: View, position: Int, id: Long) {
+                Toast.makeText(activity,
+                        getString(R.string.selected_item) + " " +
+                                "" + filesizes[position], Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                // write code to perform some action
+            }
+        }
     }
 
     override fun onAttach(context: Context) {
