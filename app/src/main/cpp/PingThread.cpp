2ff5d68f30ae1749bd0239edde3a1464cfe5490e
Arvind Umrao <arvind.umrao@sococo.com>
2019-12-10 12:14:42 +0000

    removed ssl

---
+++ app/src/main/cpp/PingThread.cpp
@@ -0,0 +1,5 @@
+//
+// Created by root on 10/12/19.
+//
+
+#include "PingThread.h"
---
+++ app/src/main/cpp/PingThread.h
@@ -0,0 +1,16 @@
+//
+// Created by root on 10/12/19.
+//
+
+#ifndef MEDIAAPP_PINGTHREAD_H
+#define MEDIAAPP_PINGTHREAD_H
+
+
+
+class PingThread {
+
+};
+
+
+
+#endif //MEDIAAPP_PINGTHREAD_H
