deb98d257fb1b0b73e727fea3c0d9589aa6efd6d
Parashurama G D <parashurama.govind@harman.com>
2020-01-08 17:07:38 +0530

    Video view live streaming.

--- app/src/main/java/com/harman/vns/LiveStreamingFragment.kt
+++ app/src/main/java/com/harman/vns/LiveStreamingFragment.kt
@@ -6,19 +6,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.harman.vns.databinding.FragmentLiveStreamingBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class LiveStreamingFragment : Fragment() {
 
+    lateinit var binding: FragmentLiveStreamingBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_live_streaming, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_live_streaming, container, false)
+        initView()
+        return binding.root
     }
 
-
+    private fun initView() {
+        binding.startVideo.setOnClickListener {
+            binding.videoView.setVideoPath("https://www.radiantmediaplayer.com/media/bbb-360p.mp4")
+            binding.videoView.start()
+        }
+        binding.stopVideo.setOnClickListener {
+            binding.videoView.stopPlayback()
+        }
+    }
 }
